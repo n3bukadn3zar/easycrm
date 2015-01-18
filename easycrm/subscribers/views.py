978069348107af2ab15e4f7e1d280a3b39fd5494
n3bukadn3zar <n3bukadn3zar@gmail.com>
2015-01-18 22:18:41 +0100

    updated the models and view

--- easycrm/subscribers/views.py
+++ easycrm/subscribers/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 
 from .forms import SubscriberForm
+from .models import Subscriber
 
 def subscriber_new(request, template='subscribers/subscriber_new.html'):
     if request.method == 'POST':
@@ -12,11 +13,22 @@ def subscriber_new(request, template='subscribers/subscriber_new.html'):
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
             email = form.cleaned_data['email']
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
             # Create the User record
+            user = User(username=username, email=email,
+                        frist_name=first_name, lastname=last_name)
             user = User(username=username, email=email)
             user.set_password(password)
             user.save()
             # Create Subscriber Record
+            address_one = form.cleaned_data['address_one']
+            address_two = form.cleaned_data['address_two']
+            city = form.cleaned_data['city']
+            state = form.cleaned_data['state']
+            sub = Subscriber(address_one=address_one, address_two=address_two,
+                             city=city, state=state, user_rec=user)
+            sub.save()
             # Process payment (via Stripe)
             # Auto login the user
             return HttpResponseRedirect('/success/')
